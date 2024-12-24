517dacf13c5fef01e6cdf1b333b64453d5ad56b7
Francisco Casas <franciscojacb@gmail.com>
2024-12-23 22:28:36 -0300

    Add minshell.html.

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ source emsdk/emsdk_env.sh
 
 emcc -o jolly.html src/main.c -Os -Wall raylib/src/libraylib.a \
   -I. -Iraylib/src/ -L. -Lraylib/src/ -s USE_GLFW=3 -s ASYNCIFY \
-  -DPLATFORM_WEB
+  --shell-file minshell.html -DPLATFORM_WEB
 
 mv jolly.html index.html
 zip index.zip index.html jolly.js jolly.wasm
