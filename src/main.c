867cf80b94f4751233fca75408d95c894174f65d
Francisco Casas <franciscojacb@gmail.com>
2024-12-24 00:52:44 -0300

    Basic layout.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,104 @@
-#include "raylib.h"
+#include <raylib.h>
+
+#include "palettes.h"
+
+#define CANVAS_SIZE 32
+
+struct layout
+{
+    bool vertical;
+    int scale;
+    Rectangle canvas;
+    Rectangle palette;
+    Rectangle current[2];
+    Rectangle buttons[2];
+};
+
+void rectangle_scale(Rectangle *rect, int offset_x, int offset_y, int scale)
+{
+    rect->x *= scale;
+    rect->y *= scale;
+    rect->width *= scale;
+    rect->height *= scale;
+    rect->x += offset_x;
+    rect->y += offset_y;
+}
+
+Rectangle rect_grow(Rectangle rect, int growth)
+{
+    rect.x -= growth;
+    rect.y -= growth;
+    rect.width += 2*growth;
+    rect.height += 2*growth;
+    return rect;
+}
+
+struct layout compute_layout(bool vertical)
+{
+    struct layout lay = {0};
+    lay.vertical = vertical;
+
+    int size_w = GetScreenWidth();
+    int size_h = GetScreenHeight();
+
+    int required_w = 1 + 32*2 + 1 + (vertical ? 0 : 2*2 + 1);
+    int required_h = 1 + 32*2 + 1 + (vertical ? 2*2 + 1 : 0);
+
+    int scale_w = size_w / required_w;
+    int scale_h = size_h / required_h;
+    int scale = (scale_w < scale_h) ? scale_w : scale_h;
+    lay.scale = scale;
+
+    int offset_x = (size_w - scale * required_w)/2;
+    int offset_y = (size_h - scale * required_h)/2;
+
+    lay.canvas.x = 1;
+    lay.canvas.y = 1;
+    lay.canvas.width = 32*2;
+    lay.canvas.height = 32*2;
+
+    if (vertical)
+    {
+        lay.current[0].x = 1;
+        lay.current[0].y = 1 + 32*2 + 1;
+        lay.current[0].width = 4;
+        lay.current[0].height = 4;
+
+        lay.current[1].x = 1;
+        lay.current[1].y = 1 + 4 + 1 + 32*2 + 1;
+        lay.current[1].width = 4;
+        lay.current[1].height = 4;
+
+        lay.palette.x = 1 + 4 + 1 + 4 + 1;
+        lay.palette.y = 1 + 32*2 + 1;
+        lay.palette.width = 16*2;
+        lay.palette.height = 2*2;
+    }
+    else
+    {
+        lay.current[0].x = 1 + 32*2 + 1;
+        lay.current[0].y = 1;
+        lay.current[0].width = 4;
+        lay.current[0].height = 4;
+
+        lay.current[1].x = 1 + 32*2 + 1;
+        lay.current[1].y = 1 + 4 + 1;
+        lay.current[1].width = 4;
+        lay.current[1].height = 4;
+
+        lay.palette.x = 1 + 32*2 + 1;
+        lay.palette.y = 1 + 4 + 1 + 4 + 1;
+        lay.palette.width = 2*2;
+        lay.palette.height = 16*2;
+    }
+
+    rectangle_scale(&lay.canvas, offset_x, offset_y, scale);
+    rectangle_scale(&lay.current[0], offset_x, offset_y, scale);
+    rectangle_scale(&lay.current[1], offset_x, offset_y, scale);
+    rectangle_scale(&lay.palette, offset_x, offset_y, scale);
+
+    return lay;
+}
 
 int main(void)
 {
@@ -8,17 +108,71 @@ int main(void)
 
     SetTargetFPS(60);
 
+    int cells[CANVAS_SIZE][CANVAS_SIZE] = {0};
+    int pal = 0; // Current palette
+
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
+        struct layout layout_v = compute_layout(true);
+        struct layout layout_h = compute_layout(false);
+        struct layout layout = (layout_v.scale >= layout_h.scale) ? layout_v : layout_h;
+
         // Update
 
         // Draw
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
+            DrawRectangleLinesEx(rect_grow(layout.canvas, 1), 1, DARKGRAY);
+            DrawRectangleLinesEx(rect_grow(layout.palette, 1), 1, DARKGRAY);
+            DrawRectangleLinesEx(rect_grow(layout.current[0], 1), 1, DARKGRAY);
+            DrawRectangleLinesEx(rect_grow(layout.current[1], 1), 1, DARKGRAY);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            for (int y = 0; y < CANVAS_SIZE; ++y)
+            {
+                for (int x = 0; x < CANVAS_SIZE; ++x)
+                {
+                    Rectangle r;
+                    r.x = layout.canvas.x + 2 * layout.scale * x;
+                    r.y = layout.canvas.y + 2 * layout.scale * y;
+                    r.width = 2*layout.scale;
+                    r.height = 2*layout.scale;
+
+                    int col = cells[y][x];
+
+                    DrawRectangleRec(r, GetColor(palettes[pal].colors[col]));
+                }
+            }
+
+            // Draw palette
+            for (int c = 0; c < 16; ++c)
+            {
+                Rectangle r = layout.palette;
+                if (layout.vertical)
+                {
+                    r.width /= 16;
+                    r.x += r.width * c;
+                }
+                else
+                {
+                    r.height /= 16;
+                    r.y += r.height * c;
+                }
+                DrawRectangleRec(r, GetColor(palettes[pal].colors[c]));
+            }
+
+            // Draw grid
+            for (int x = 0; x < CANVAS_SIZE; ++x)
+            {
+                int px = layout.canvas.x + 2*layout.scale*x + 1;
+                DrawLine(px, layout.canvas.y, px, layout.canvas.y + layout.canvas.height, GRAY);
+            }
+            for (int y = 0; y < CANVAS_SIZE; ++y)
+            {
+                int py = layout.canvas.y + 2*layout.scale*y;
+                DrawLine(layout.canvas.x, py, layout.canvas.x + layout.canvas.width, py, GRAY);
+            }
 
         EndDrawing();
     }
---
+++ src/palettes.h
@@ -0,0 +1,31 @@
+
+#define PALETTE_COUNT 1
+
+static struct palette
+{
+    const char *name;
+    unsigned int colors[16];
+} palettes[PALETTE_COUNT] =
+{
+    {
+        "Lump 16",
+        {
+            0x000000FF,
+            0x20123bFF,
+            0x451c33FF,
+            0xa8274bFF,
+            0xed7332FF,
+            0xfff167FF,
+            0xffffd9FF,
+            0x7defffFF,
+            0x2dd660FF,
+            0x2c7cc7FF,
+            0x3030a1FF,
+            0x381378FF,
+            0x8c2da6FF,
+            0xf57fc6FF,
+            0xffd7a8FF,
+            0xc8765cFF,
+        }
+    }
+};
